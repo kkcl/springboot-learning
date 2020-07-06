1339902a8ad709e932ebd3affa2049f35f5618b5
kkcl <2096648710@qq.com>
2020-07-06 18:31:30 +0800

    SpringBoot2.x入门案例

---
+++ src/main/java/com/kkcl/springboot/learning/SpringbootLearningApplication.java
@@ -0,0 +1,14 @@
+package com.kkcl.springboot.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearningApplication.class, args);
+    }
+
+
+}
